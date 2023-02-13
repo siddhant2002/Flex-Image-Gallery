05a57dfa1625832aaea19a04771939ef0bc0d341
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-02-13 23:49:07 +0530

    Files added

---
+++ alpha1.js
@@ -0,0 +1,18 @@
+const audio = new Audio('s1.mp3');
+audio.play();
+const audio1 = new Audio('s2.mp3');
+audio1.play();
+const panels = document.querySelectorAll('.panel');
+function toggleOpen()
+{
+    this.classList.toggle('open');
+}
+function toggleActive(e)
+{
+    if(e.propertyName.includes('flex'))
+    {
+        this.classList.toggle('open-active');
+    }
+}
+panels.forEach(panel => panel.addEventListener('click',toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend',toggleActive));
