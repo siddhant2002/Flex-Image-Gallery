9dda644dcd06370041b4d4840b723e455a4df17c
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-02-13 23:51:58 +0530

    Files added

--- alpha1.js
+++ alpha1.js
@@ -1,18 +1,20 @@
-const audio = new Audio('s1.mp3');
-audio.play();
-const audio1 = new Audio('s2.mp3');
-audio1.play();
+function play() {
+    const audio = new Audio('s1.mp3');
+    audio.play();
+    const audio1 = new Audio('s2.mp3');
+    audio1.play();
+}
+setInterval(play, 1000)
+
+
 const panels = document.querySelectorAll('.panel');
-function toggleOpen()
-{
+function toggleOpen() {
     this.classList.toggle('open');
 }
-function toggleActive(e)
-{
-    if(e.propertyName.includes('flex'))
-    {
+function toggleActive(e) {
+    if (e.propertyName.includes('flex')) {
         this.classList.toggle('open-active');
     }
 }
-panels.forEach(panel => panel.addEventListener('click',toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend',toggleActive));
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
